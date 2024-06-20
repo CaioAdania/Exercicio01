81c7d5f45cc55f28054f8e363cdfdd9db5844060
CaioAdania <caioadania@gmail.com>
2024-06-20 14:00:20 -0300

    Exercicio

---
+++ Exercicio01/Entities/HourContract.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio01.Entities
+{
+    internal class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePorHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+
+        }
+
+        public HourContract(DateTime date, double valuePorHour, int hours)
+        {
+            Date = date;
+            ValuePorHour = valuePorHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePorHour;
+        }
+    }
+}
---
+++ Exercicio01/Entities/Worker.cs
@@ -0,0 +1,48 @@
+﻿using Exercicio01.Entities.Enums;
+
+namespace Exercicio01.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }// associação
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>(); //* significa vários o que indica uma lista, esta parte é uma associação
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department) //via de regra, quando tiver uma associação "para muitos", não pode incluir no construtor.
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddContract(HourContract contract) //muito utilizado em associação de objetos
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract) //muito utilizado em associação de objetos
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+            return sum;
+        }
+    }
+}
